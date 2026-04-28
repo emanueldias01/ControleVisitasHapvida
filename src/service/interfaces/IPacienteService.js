a200d91ee2a0e2f138cf4634b4afb95fa666ebdd
KpSantiago <santiago.kauajose11@gmail.com>
2026-04-28 09:07:18 -0300

    adicionar hotfix para 'interfaces'

--- src/service/interfaces/IPacienteService.js
+++ src/service/interfaces/IPacienteService.js
@@ -1,9 +1,19 @@
 class IPacienteService {
-    async createPaciente(paciente);
-    async getAllPacientes();
-    async getPacienteById(id);
-    async updatePaciente(id, data);
-    async deletePacienteById(id);
+    async createPaciente(paciente) {
+        throw new Error("method not implemented");
+    }
+    async getAllPacientes() {
+        throw new Error("method not implemented");
+    }
+    async getPacienteById(id) {
+        throw new Error("method not implemented");
+    }
+    async updatePaciente(id, data) {
+        throw new Error("method not implemented");
+    }
+    async deletePacienteById(id) {
+        throw new Error("method not implemented");
+    }
 }
 
 export default IPacienteService;
